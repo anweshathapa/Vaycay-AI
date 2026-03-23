f4511dfa7fca69bc428a952ced4c4519898e2997
Anwesha Thapa <anweshathapa15@gmail.com>
2026-03-23 18:55:17 +0530

    fix: add google places to image config

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,36 @@
 
 // export default nextConfig;
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'plus.unsplash.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'maps.googleapis.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -71,6 +101,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'places.googleapis.com', 
         port: '',
         pathname: '/**',
       },
